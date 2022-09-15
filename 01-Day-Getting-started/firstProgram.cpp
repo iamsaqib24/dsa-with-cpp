fc2e3ec1cc488a459d09ea0b8fb11a3e8fc7cd87
Saqib <iamsaqib24@gmail.com>
2022-09-15 06:24:52 +0530

    getting started with cpp

---
+++ 01-Day-Getting-started/firstProgram.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello World" << endl;
+
+    int n = 5;
+    cout << n << endl;
+
+    float f = 1.2;
+    cout << f << endl;
+
+    double d = 1.432;
+    cout << d << endl;
+
+    char ch = 'a';
+    cout << ch << endl;
+
+    bool bl = false;
+    cout << bl << endl;
+
+    // Type Casting
+
+    int a = 'a';
+    cout << a << endl;
+
+    char c = 98;
+    cout << c << endl;
+
+    char c1 = 123456;
+    cout << c1 << endl;
+}
