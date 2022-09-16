4e8f7f88a5e43f7010ba114bfa7d0351c927e094
Saqib <iamsaqib24@gmail.com>
2022-09-16 18:45:44 +0530

    while loop

--- 02-Day-Conditions-Loops/whileLoop.cpp
+++ 02-Day-Conditions-Loops/whileLoop.cpp
@@ -9,15 +9,29 @@ int main() {
     cout << "Enter value of n : " << endl;
     cin >> n;
 
-    // Sum of all even numbers
-    if(n%2 == 0){
-        int i = 2, sum = 0;
-        while (i <= n) {
-            sum += i;
-            i = i +2;
+    // find prime or not
+    int i = 2;
+    while (i < n) {
+        if (n % i == 0){
+            cout << "Entered number is prime " << endl;
+            break;
         }
-        cout << "Sum of all even numbers is : " << sum << endl;
+        else {
+            cout << "Entered number is not prime " << endl;
+            break;
+        }
+        i++;
     }
+
+    // Sum of all even numbers
+    // if(n%2 == 0){
+    //     int i = 2, sum = 0;
+    //     while (i <= n) {
+    //         sum += i;
+    //         i = i +2;
+    //     }
+    //     cout << "Sum of all even numbers is : " << sum << endl;
+    // }
 
     // Sum of N numbers
     // int i = 1, sum = 0;
