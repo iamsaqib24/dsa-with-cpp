401b0045adde541dbd5dafe2b4af706309fbca0d
Saqib <iamsaqib24@gmail.com>
2022-09-19 22:14:19 +0530

    pattern-3

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -25,11 +25,42 @@
 //     }
 // }
 
+
+
 // Pattern - 2
 /*
     1 1 1
     2 2 2
     3 3 3
+*/
+
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int n, i = 1;
+//     cout << "Enter the value of n : " << endl;
+//     cin >> n;
+
+//     while (i <= n) {
+//         int j = 1;
+//         while (j <= n) {
+//             cout << i << " ";
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+// }
+
+
+
+// Pattern - 3
+/*
+    1 2 3 4
+    1 2 3 4
+    1 2 3 4
+    1 2 3 4
 */
 
 #include<iostream>
@@ -42,11 +73,15 @@ int main() {
 
     while (i <= n) {
         int j = 1;
-        while (j <= n) {
-            cout << i << " ";
+        while (j <=n) {
+            cout << j << " ";
             j++;
         }
         cout << endl;
         i++;
     }
 }
+
+
+
+// Pattern - 4
