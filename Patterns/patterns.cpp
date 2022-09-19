2ff9dba6b3132a87ca6c78ea7bf2d7b67a0902b6
Saqib <iamsaqib24@gmail.com>
2022-09-19 22:05:12 +0530

    pattern-2

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -6,18 +6,44 @@
     * * * *
 */
 
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int n, i = 1;
+//     cout << "Enter the value n : " << endl;
+//     cin >> n;
+
+//     while(i <= n) {
+//         int j = 1;
+//         while(j <= n) {
+//             cout << "*" << " ";
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+// }
+
+// Pattern - 2
+/*
+    1 1 1
+    2 2 2
+    3 3 3
+*/
+
 #include<iostream>
 using namespace std;
 
 int main() {
     int n, i = 1;
-    cout << "Enter the value n : " << endl;
+    cout << "Enter the value of n : " << endl;
     cin >> n;
 
-    while(i <= n) {
+    while (i <= n) {
         int j = 1;
-        while(j <= n) {
-            cout << "*" << " ";
+        while (j <= n) {
+            cout << i << " ";
             j++;
         }
         cout << endl;
