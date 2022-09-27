4186786da68605bbfc95cbc22f1fb5e5889167f4
Saqib <iamsaqib24@gmail.com>
2022-09-27 12:31:45 +0530

    ok

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -63,25 +63,50 @@
     1 2 3 4
 */
 
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int n, i = 1;
+//     cout << "Enter the value of n : " << endl;
+//     cin >> n;
+
+//     while (i <= n) {
+//         int j = 1;
+//         while (j <=n) {
+//             cout << j << " ";
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+// }
+
+
+
+// Pattern - 4
+/*
+   1 2 3
+   4 5 6
+   7 8 9
+*/
+
 #include<iostream>
 using namespace std;
 
 int main() {
-    int n, i = 1;
+    int n, i = 1, count = 1;
     cout << "Enter the value of n : " << endl;
     cin >> n;
 
     while (i <= n) {
         int j = 1;
-        while (j <=n) {
-            cout << j << " ";
+        while (j <= n) {
+            cout << " ";
+            count++;
             j++;
         }
         cout << endl;
         i++;
     }
 }
-
-
-
-// Pattern - 4
