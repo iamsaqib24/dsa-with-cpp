04818d488a956e3d81d624cd7591b45b047c5523
Saqib <iamsaqib24@gmail.com>
2022-09-28 13:58:12 +0530

    binarySearch

--- 09-Day-Binary-Search/binarySearch.cpp
+++ 09-Day-Binary-Search/binarySearch.cpp
@@ -2,6 +2,31 @@
 
 using namespace std;
 
+int binarySearch(int arr[], int size, int target) {
+
+    int start = 0;
+    int end = size - 1;
+
+    while(start <= end) {
+        int mid = start + (end - start)/2;
+
+        if(arr[mid] == target) {
+            return mid;
+        }
+        else if(arr[mid] > target) {
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
+    }
+    return - 1;
+}
+
 int main() {
 
+    int arr[8] = {2,4,6,8,9,11,21,25};
+
+
+    cout << "Index value of my target is : " << binarySearch(arr, 8, 2) << endl;
 }
